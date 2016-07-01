0faef767b5f38ab202d45d3e8bc3a5369d5ff964
justinzwick <zwickyj@gmail.com>
2016-07-01 17:44:41 -0400

    added onClickListener that switches to ResultsActivity

--- app/src/main/java/justinzwick/reactiontimer/ClickActivity.java
+++ app/src/main/java/justinzwick/reactiontimer/ClickActivity.java
@@ -1,13 +1,35 @@
 package justinzwick.reactiontimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.pixplicity.easyprefs.library.Prefs;
 
 public class ClickActivity extends AppCompatActivity {
+    private static final String ELAPSED_TIME_KEY = "elapsed time key";
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.greenLayout);
+
+
+        mRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ClickActivity.this, ResultsActivity.class);
+
+
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
