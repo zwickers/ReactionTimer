a2f7d471542fdfec51445a61e69ad36d0e03fa45
Justin Zwick <justinzwick@Justins-MBP.fios-router.home>
2016-07-01 16:37:40 -0400

    Initial commit

---
+++ app/src/main/java/justinzwick/reactiontimer/ClickActivity.java
@@ -0,0 +1,13 @@
+package justinzwick.reactiontimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_click);
+    }
+}
---
+++ app/src/main/java/justinzwick/reactiontimer/PrefsApplication.java
@@ -0,0 +1,7 @@
+package justinzwick.reactiontimer;
+
+import android.app.Application;
+
+public class PrefsApplication extends Application {
+
+}
