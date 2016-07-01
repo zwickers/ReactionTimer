04aa482f9427e545c7c53858ec1f14947e16ef56
justinzwick <zwickyj@gmail.com>
2016-07-01 17:56:22 -0400

    setText() of mMillisecondTextView to the correct number of milliseconds from ClickActivity

--- app/src/main/java/justinzwick/reactiontimer/ResultsActivity.java
+++ app/src/main/java/justinzwick/reactiontimer/ResultsActivity.java
@@ -1,13 +1,35 @@
 package justinzwick.reactiontimer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.pixplicity.easyprefs.library.Prefs;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private static final String ELAPSED_TIME_KEY = "elapsed time key";
+    private RelativeLayout mRelativeLayout;
+    private TextView mMillisecondTextView, mAverageMillisecondsLabel, mAttemptsLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.resultsLayout);
+        mMillisecondTextView = (TextView) findViewById(R.id.millisecondTextView);
+        mAverageMillisecondsLabel = (TextView) findViewById(R.id.averageMilliseconds);
+        mAttemptsLabel = (TextView) findViewById(R.id.attemptsLabel);
+
+        Intent pastIntent = getIntent();
+        long milliseconds = pastIntent.getLongExtra(ELAPSED_TIME_KEY,0);
+
+        mMillisecondTextView.setText(milliseconds + " milliseconds");
+
+
     }
 }
