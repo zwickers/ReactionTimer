14a223cfe92960e13e0cc6372b0fd9dd3806a103
justinzwick <zwickyj@gmail.com>
2016-07-01 17:07:04 -0400

    set onclicklistener on the relative layout that switches to the WaitActivity

--- app/src/main/java/justinzwick/reactiontimer/StartingActivity.java
+++ app/src/main/java/justinzwick/reactiontimer/StartingActivity.java
@@ -1,13 +1,32 @@
 package justinzwick.reactiontimer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.pixplicity.easyprefs.library.Prefs;
 
 public class StartingActivity extends AppCompatActivity {
+
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.startLayout);
+        mRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartingActivity.this,WaitActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
