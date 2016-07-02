147b427e88ef7a19435f542cbea6778e201a3b0f
justinzwick <zwickyj@gmail.com>
2016-07-01 20:05:50 -0400

    initialize attempts and total time as 0 in the StartingActivity

--- app/src/main/java/justinzwick/reactiontimer/StartingActivity.java
+++ app/src/main/java/justinzwick/reactiontimer/StartingActivity.java
@@ -12,11 +12,16 @@ import com.pixplicity.easyprefs.library.Prefs;
 public class StartingActivity extends AppCompatActivity {
 
     private RelativeLayout mRelativeLayout;
+    public static final String ATTEMPTS_KEY = "attempts key";
+    public static final String TOTAL_TIME_KEY = "total time key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+
+        Prefs.putInt(ATTEMPTS_KEY,0);
+        Prefs.putLong(TOTAL_TIME_KEY,0);
 
         mRelativeLayout = (RelativeLayout) findViewById(R.id.startLayout);
         mRelativeLayout.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +31,8 @@ public class StartingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
 
 
     }
